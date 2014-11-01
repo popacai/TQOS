4b5d86a71d1b8014c78760f169e6f6181cd3f48d
tao <popacai@gmail.com>
2014-10-31 18:04:36 -0700

    fuzz mailbox implementation

--- code/threads/mailbox.cc
+++ code/threads/mailbox.cc
@@ -29,42 +29,62 @@ Mailbox::~Mailbox(){
 
 void Mailbox::Send(int message){
     mailbox_lock->Acquire();
+    currentThread->Yield();
     resource++;
+    currentThread->Yield();
     if (resource > 0) {
+    currentThread->Yield();
         mailbox_cond->Wait(mailbox_lock);
+    currentThread->Yield();
     }
     else{
+    currentThread->Yield();
         
         mailbox_cond->Signal(mailbox_lock);
+    currentThread->Yield();
     }
+    currentThread->Yield();
     mailbox_lock->Release();
+    currentThread->Yield();
 
     empty->P();
     currentThread->Yield();
     *buffer = message;
     currentThread->Yield();
+    currentThread->Yield();
     printf("send message is=%d\n",*buffer);
     currentThread->Yield();
+    currentThread->Yield();
     full->V();
+    currentThread->Yield();
 
 }
 
 void Mailbox::Receive(int* message){
     mailbox_lock->Acquire();
+    currentThread->Yield();
     resource--;
+    currentThread->Yield();
     if (resource < 0) {
+    currentThread->Yield();
         mailbox_cond->Wait(mailbox_lock);
+    currentThread->Yield();
     }
     else{
+    currentThread->Yield();
         mailbox_cond->Signal(mailbox_lock);
+    currentThread->Yield();
     }
     mailbox_lock->Release();
+    currentThread->Yield();
     // add another condition let the receiver to wait for sender to actually write the message to buffer after it has signaled the sender to send.
     // otherwise if the Yield is plugged between the sender Wait() and write message to buffer, the Receiver still get nothing new in the buffer.
     currentThread->Yield();
 
     full->P();
+    currentThread->Yield();
     *message = *buffer;
     currentThread->Yield();
     empty->V();
+    currentThread->Yield();
 }
