ba6b28850448965a10e2d49e07a5d68b6fdee4f4
Jiawei Chen <jic023@eng.ucsd.edu>
2014-11-14 17:08:24 -0800

    add read argument

--- code/userprog/syscommon.cc
+++ code/userprog/syscommon.cc
@@ -1,8 +1,16 @@
 #include "syscall.h"
+#include "syscommon.h"
+#include "system.h"
 
 void PushPC() {
     machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
     machine->WriteRegister(PCReg, machine->ReadRegister(PCReg) + 4);
     machine->WriteRegister(NextPCReg, machine->ReadRegister(PCReg) + 8); 
 }
+
+int read_argument(int index) {
+   int ret;
+   machine->ReadMem(machine->ReadRegister(StackReg)+16+(index-1)*4,4,&ret);
+   return ret;
+} 
 
